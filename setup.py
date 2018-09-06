ff65ca2292c7787b7186eabb8db27627189373cd
ciarancourtney <ciaran.courtney@cylon.com>
2018-09-06 15:30:43 +0100

    fix setup.py line indents

--- setup.py
+++ setup.py
@@ -14,24 +14,25 @@ changelog_file = path.join(path.dirname(__file__), "CHANGELOG.rst")
 description = open(desc_file).read() + changelog_header + open(changelog_file).read()
 
 
-setup(name='bb-pytest',
-      version=version,
-      description="Buildbot step for py.test.",
-      long_description=description,
-      classifiers=[],  # Get strings from
-                       #http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='Russell Sim',
-      author_email='russell.sim@gmail.com',
-      url='https://github.com/russell/bb-pytest',
-      license='GPL',
-      packages=find_packages(exclude=['tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+setup(
+    name='bb-pytest',
+    version=version,
+    description="Buildbot step for py.test.",
+    long_description=description,
+    classifiers=[],  # Get strings from
+    #http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Russell Sim',
+    author_email='russell.sim@gmail.com',
+    url='https://github.com/russell/bb-pytest',
+    license='GPL',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+        # -*- Entry points: -*-
+        """,
+)
