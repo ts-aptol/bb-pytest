adc9d17e568aaaaba8bb60f943e35510f204ab1d
ciarancourtney <ciaran.courtney@cylon.com>
2018-09-06 15:34:57 +0100

    Add 'buildbot' as sole dist dependency and repurpose requirements.txt for development only
    Note we are not making pytest a package dependency as its not strictly required

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        # -*- Extra requirements: -*-
+        'buildbot'
     ],
     entry_points="""
         # -*- Entry points: -*-
